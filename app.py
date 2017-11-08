ea98d14377bd97d355194d6ac3cc20929ad6ee6e
chrisyang <chrispink@foxmail.com>
2017-11-08 22:39:38 +0800

    Success in Query

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy
 import logging
 
 app = Flask('chris')
+
 
 @app.route('/')
 @app.route('/<name>')
---
+++ ormtest.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:940425@localhost:3306/test?charset=utf8'
+
+db = SQLAlchemy(app)
+
+class actor(db.Model):
+	__tablename__ = 'actor'
+
+	actor_id = db.Column(db.Integer, primary_key=True)
+	first_name = db.Column(db.String(20))
+	last_name = db.Column(db.String(20))
+	last_update = db.Column(db.String(20))
+
+	def __init__(self, actor_id, first_name, last_name, last_update):
+		self.actor_id = actor_id
+		self.first_name = first_name
+		self.last_name = last_name
+		self.last_name = last_update
+
+	def __repr__(self):
+		return 'Actor_id:{} \nFirst_name:{} \nLast_name:{} \nLast_update:{}'.format(self.actor_id,self.first_name,self.last_name,self.last_update)
+
+act = actor.query.filter(actor.actor_id == '4').one()
+print(act)
