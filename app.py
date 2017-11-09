42e282348395057575fe5ade401d52cc8834d8a9
chrisyang <chrispink@foxmail.com>
2017-11-09 16:32:15 +0800

    basic query

--- app.py
+++ app.py
@@ -1,14 +1,27 @@
-from flask import Flask,render_template
+from flask import Flask,render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from ormtest import actor,db,app
 import logging
 
-app = Flask('chris')
+#app = Flask('chris')
+#应考虑app要在哪里实例化
 
-
-@app.route('/')
-@app.route('/<name>')
+@app.route('/index')
+@app.route('/index/<name>')
 def hello_chris(name = None):
 	return render_template('hello.html',name = name)
+
+#11.9 methods错写成method
+@app.route('/check',methods = ['GET','POST'])
+def check():
+	if request.method == 'POST':
+		actor_id = request.form.get('id',None)
+		act = actor.query.filter(actor.actor_id == str(actor_id)).one()
+		return render_template('show.html',actor = act)
+	else:
+		return render_template('check.html')
+		#11.9 忘了写return ValueError: View function did not return a response
+
 
 app.run(port=8000,debug=True)
 logging.info('Server is started at 8000')
--- ormtest.py
+++ ormtest.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:940425@localhost:3306/test?charset=utf8'
-
+#11.8 理解"数据库驱动" 默认MySQLdb在python 3已不支持 需手动改为mysqlconnector或pymysql
 db = SQLAlchemy(app)
 
 class actor(db.Model):
@@ -23,5 +23,5 @@ class actor(db.Model):
 	def __repr__(self):
 		return 'Actor_id:{} \nFirst_name:{} \nLast_name:{} \nLast_update:{}'.format(self.actor_id,self.first_name,self.last_name,self.last_update)
 
-act = actor.query.filter(actor.actor_id == '4').one()
-print(act)
+#act = actor.query.filter(actor.actor_id == '4').one()
+#print(act)
